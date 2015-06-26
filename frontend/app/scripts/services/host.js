44268e3b61acdb9c56f120574cc0fca65412f9d2
Łukasz Kracoń <lukasz.kracon@jcore.pl>
2015-06-26 10:51:19 +0200

    for openshift

--- frontend/app/scripts/services/host.js
+++ frontend/app/scripts/services/host.js
@@ -1,8 +1,14 @@
 'use strict';
-
-angular.module('frontendApp')
-  .factory('Host', function () {
-    return {
-      backend: "http://localhost:8080/scheduler/api"
-    };
-  });
+angular.module('frontendApp').factory('Host', function() {
+    if (window.location.host === 'localhost:9000') {
+        /* developer settings - run from localhost on ports: frontend - 9000, backend - 8080 */
+        return {
+            backend: 'http://localhost:8080/scheduler/api'
+        };
+    } else {
+        /* production settings - all on 8080 */
+        return {
+            backend: 'http://' + window.location.host.replace('9000', '8080') + '/zmianoustalacz/api'
+        };
+    }
+});
